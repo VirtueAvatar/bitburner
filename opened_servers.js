bf21a8721e496a55d5783ae0fa08e2bc6ad5bef5
VirtueAvatar <avatar.1@gmail.com>
2022-04-21 09:34:24 +1000

    Create opened_servers.js
    required for sharing_is_caring.js

---
+++ opened_servers.js
@@ -0,0 +1,24 @@
+function scan(ns, parent, server, list) {
+    const children = ns.scan(server);
+    for (let child of children) {
+        if (parent == child) {
+            continue;
+        }
+        list.push(child);
+        scan(ns, server, child, list);
+    }
+}
+export function list_servers(ns) {
+    const list = [];
+    scan(ns, '', 'home', list);
+    return list;
+}
+
+/** @param {NS} ns **/
+export async function main(ns) {
+    ns.tprint(list_servers(ns));
+}
+
+export function autocomplete(data, args) {
+    return data.servers;
+}
